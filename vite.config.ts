ed0a2702ee3458fe521335828c4fb286b958552b
enter-pro <github@enter.pro>
2026-06-25 04:33:29 +0000

    build(config): add alias for get-nonce module

--- vite.config.ts
+++ vite.config.ts
@@ -19,6 +19,7 @@ export default defineConfig(({ mode }) => {
     resolve: {
       alias: {
         "@": path.resolve(__dirname, "./src"),
+        "get-nonce": path.resolve(__dirname, "node_modules/get-nonce"),
       },
     },
     base: '/',
